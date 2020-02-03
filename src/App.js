10df27aa900ad7270fbc242da7fe61b12a582692
Cedric <cedric.balmer@gmx.ch>
2020-02-03 13:33:42 -0800

    console.log the input

--- src/App.js
+++ src/App.js
@@ -18,6 +18,17 @@ const particlesOption = {
   }
 }
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
   render() {
   return (
     <div className="App">
@@ -26,7 +37,7 @@ class App extends Component {
       <Navigation/>
       <Logo/>
       <Rank/>
-      <ImageLinkForm/>
+      <ImageLinkForm onInputChange={this.onInputChange}/>
      {/* <FaceRecognition/> */}
     </div>
   )};
